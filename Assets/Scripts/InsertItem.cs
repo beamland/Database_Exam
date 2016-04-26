45a796ab5a0598334cfba755053b306660bd1618
beamland <samakute_111@hotmail.com>
2016-04-26 15:08:19 +0700

    Exam

---
+++ Assets/Scripts/InsertItem.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using Mono.Data.SqliteClient;
+using System.Data;
+using System;
+using UnityEngine.UI;
+
+public class InsertItem : MonoBehaviour {
+
+	public InputField nameInput;
+	public InputField passwordInput;
+    public InputField ageInput;
+    public Text Showtxt;
+
+    void Update()
+    {
+        Select();
+    }
+
+	public void Insert(){
+		string connectionString = "URI=file:" + Application.dataPath + "/myDatabase.db"; //Path to database.
+		IDbConnection dbconn;
+		dbconn = (IDbConnection) new SqliteConnection(connectionString);
+
+		dbconn.Open(); //Open connection to the database.
+
+		IDbCommand dbcmd = dbconn.CreateCommand();
+
+		string sqlQuery = "insert into Account (userName,password,Age) values ('"+ nameInput.text +"','"+ passwordInput.text +"','"+ ageInput.text +"')";
+		dbcmd.CommandText = sqlQuery;
+
+		IDataReader reader = dbcmd.ExecuteReader();
+
+		reader.Close();reader = null;
+		dbcmd.Dispose();dbcmd = null;
+		dbconn.Close();dbconn = null;
+	}
+
+    public void Select()
+    {
+        //int accountID = FindAccountID();
+        string connectionString = "URI=file:" + Application.dataPath + "/myDatabase.db"; //Path to database.
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(connectionString);
+        dbconn.Open(); //Open connection to the database.
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        string sqlQuery = "SELECT * FROM Account";
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+        Showtxt.text = "";
+        while (reader.Read())
+        {
+            string UsernameList = reader.GetString(1);
+            string PasswordList = reader.GetString(2);
+            int AgeList = reader.GetInt32(3);
+            Showtxt.text += UsernameList + " - " + PasswordList + " - " + AgeList + "\n";
+
+        }
+        reader.Close(); reader = null;
+        dbcmd.Dispose(); dbcmd = null;
+        dbconn.Close(); dbconn = null;
+    }
+
+}
